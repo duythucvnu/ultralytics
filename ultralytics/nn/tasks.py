f9236cdc182597ccd370481be962e8abc79f61c3
Thuc Nguyen Duy <nguyenduythuc1608@gmail.com>
2025-06-12 17:17:55 +0700

    Update tasks.py
    Signed-off-by: Thuc Nguyen Duy <nguyenduythuc1608@gmail.com>

--- ultralytics/nn/tasks.py
+++ ultralytics/nn/tasks.py
@@ -69,6 +69,7 @@ from ultralytics.nn.modules import (
     YOLOEDetect,
     YOLOESegment,
     v10Detect,
+    SelectItem,
 )
 from ultralytics.utils import DEFAULT_CFG_DICT, DEFAULT_CFG_KEYS, LOGGER, YAML, colorstr, emojis
 from ultralytics.utils.checks import check_requirements, check_suffix, check_yaml
